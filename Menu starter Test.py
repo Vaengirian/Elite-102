52c9bfc1376f34426ba9a38e57aed9e6b854d2c7
Vaengirian <ianallenchung@gmail.com>
2023-04-07 17:14:06 -0500

    Adding new file

---
+++ Menu starter Test.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+class App(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.pack(expand=1)
+        
+        frm = ttk.Frame(self, padding=10)
+        frm.grid()
+        ttk.Label(frm, text="Welcome to [Corporation Name]!").grid(column=0, row=0)
+        ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=1)
+        ttk.Button(frm, text="Deposit", command=root.destroy).grid(column=0, row=2)
+        ttk.Button(frm, text="Withdraw", command=root.destroy).grid(column=0, row=3)
+        ttk.Button(frm, text="View", command=root.destroy).grid(column=0, row=4)
+        
+
+# create the application
+root = tk.Tk()
+myapp = App(root)
+
+#
+# here are method calls to the window manager class
+#
+myapp.master.title("[Corporation Name]")
+myapp.master.maxsize(1000, 400)
+
+# start the program
+myapp.mainloop()
