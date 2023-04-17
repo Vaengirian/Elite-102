384d23a1214179ee4ca04facf72c3d35b3f38537
Vaengirian <ianallenchung@gmail.com>
2023-04-16 22:18:12 -0500

    Adding new file

--- Function Call Test.py
+++ Function Call Test.py
@@ -5,8 +5,11 @@ window_main.geometry("400x200")
  
 def submitFunction():
     print('Submit button is clicked.')
+
+def sndfunction():
+    print('2nd function successful')
  
-button_submit = tkinter.Button(window_main, text ="Submit", command=submitFunction)
+button_submit = tkinter.Button(window_main, text ="Submit", command=lambda: [submitFunction(),sndfunction()])
 button_submit.config(width=20, height=2)
  
 button_submit.pack()
--- IC Banking App.py
+++ IC Banking App.py
@@ -1,25 +1,47 @@
 import mysql.connector
 import tkinter as tk
 from tkinter import *
-from tkinter import ttk
+connection = mysql.connector.connect(user = 'modbankapp', database = 'bankapp', password = 'M3k5!tsr693^')
+cursor = connection.cursor()
 class App(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
         self.pack(expand=1)
-        
-        frm = ttk.Frame(self, padding=10)
-        frm.grid()
-        ttk.Label(frm, text="Welcome to [Corporation Name]!").grid(column=0, row=0)
-        ttk.Button(frm, text="Create account", command=root.destroy).grid(column=0, row=1)
-        ttk.Button(frm, text="Log in",command=root.destroy).grid(column=0, row=2)
+        label1 = tk.Label(root2, text="Welcome to [Corporation Name]!")
+        label1.pack()
+        caccount = tk.Button(root2, text="Create account", command=root2.destroy)
+        caccount.pack()
+        logine = tk.Button(root2, text="Log in",command=App.logininterface)
+        logine.pack()
         #ttk.Button(frm, text="View", command=root.destroy).grid(column=0, row=2)
         #ttk.Button(frm, text="Deposit", command=root.destroy).grid(column=0, row=3)
         #ttk.Button(frm, text="Withdraw", command=root.destroy).grid(column=0, row=4)
-        ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=3)
-
-root = tk.Tk()
-myapp = App(root)
-
+        quite = tk.Button(root2, text="Quit", command=App.connectionsever)
+        quite.pack()
+        #a button
+        #ttk.Button(frm, text="a", command=Appfunction.a).grid(column=0, row=4)
+    def connectionsever():
+        cursor.close()
+        connection.close()
+        root2.destroy()
+    def createaccount():
+        bankusername = input("Enter a username\n") #transfer to tkinter window somehow
+        bankpassword = input("Enter a password\n") #transfer to tkinter window somehow (widgets)
+    def printInput(text):
+        inp = text.get(1.0, "end-1c")
+        print(inp)
+        print('d')
+    def logininterface():
+        #root2.geometry('600x250') #geometry restructure
+        for widgets in root2.winfo_children(): #deletes all widgets
+            widgets.destroy()
+        inputtxt = tk.Text(root2,height = 5,width = 20)
+        inputtxt.pack()
+        printButton = tk.Button(root2, text = "Print", command = lambda: App.printInput(inputtxt))
+        printButton.pack()
+root2 = tk.Tk()
+root2.geometry('1000x400')
+myapp = App(root2)
 #
 # here are method calls to the window manager class
 #
